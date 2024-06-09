65cb0aae08b6995fc9db0fe0a82524f5171c1f46
Sofia <sofiaantenor1418@gmail.com>
2024-06-09 18:09:06 -0300

    Anatomia-das-Classes-2

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,6 +1,20 @@
+// Aula 1 - Anatomia de Classes 
 public class MinhaClasse {
     
     public static void main(String[] args) {
+        
         System.out.print("Olá Turma, sejam bem-vindos");
+
+        // Aula 2 - Anatomia de Classes 
+        final String BR = "Brasil"; //variável imutável - "final", ou letras maiúscula
+        // declaralção de variáveis válidas
+        int numero$um = 1;
+        int numero1 = ~2;
+        int numeroum =3;
+        int longo = 4;
+
+
+
+
     }
 }
