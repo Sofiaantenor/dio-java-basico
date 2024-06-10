28166b96a07d0ac69d13e52d18f5057180007d96
Sofia <sofiaantenor1418@gmail.com>
2024-06-10 17:39:06 -0300

    Anatomia-deClasses-3

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -3,18 +3,30 @@ public class MinhaClasse {
     
     public static void main(String[] args) {
         
-        System.out.print("Olá Turma, sejam bem-vindos");
+        //System.out.print("Olá Turma, sejam bem-vindos");
 
         // Aula 2 - Anatomia de Classes 
-        final String BR = "Brasil"; //variável imutável - "final", ou letras maiúscula
+        //final String BR = "Brasil"; //variável imutável - "final", ou letras maiúscula
         // declaralção de variáveis válidas
-        int numero$um = 1;
-        int numero1 = ~2;
-        int numeroum =3;
-        int longo = 4;
+        //int numero$um = 1;
+        //int numero1 = ~2;
+        //int numeroum =3;
+        //int longo = 4;
 
+        // Aula 3 - Anatomia de Classes 
+        //String meuNome = "Sofia";
+        //int anoFrabricacao = 2024;
+        //boolean verdadeira = false;
+        //anoFrabricacao = 2005;
 
-
+        String primeiroNome = "Sofia";
+        String segundoNome = "Teixeira";
+        String nomeCompleto = nomeCompleto (primeiroNome,segundoNome);
+        System.out.println(nomeCompleto);
 
     }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome){
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+    } 
 }
